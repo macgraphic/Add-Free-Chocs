03efa6efe564fc883162259c6afc0ab5ecb2e586
Mark Smallman <mark@macgraphic.co.uk>
2017-01-06 11:33:09 +0000

    Update add-free-chocs.php

--- add-free-chocs.php
+++ add-free-chocs.php
@@ -6,7 +6,7 @@
  * Description: Add Free Chocolates to cart when an item from the flower-bunch category is ordered.
  * Author: Mark Smallman | GraphicAndWeb.com
  * Author URI: http://www.graphicandweb.com
- * License: GPL3
+ * License: GNU GPL v3
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
